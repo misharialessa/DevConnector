47a53e61030eb6f48ee71d822969fcc135547fff
Mishari <shamiya@gmail.com>
2020-04-03 21:41:35 +0300

    User Authentication Route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,86 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 // @route    GET api/auth
 // @desc     Test route
 // @access   Public
 
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(error.message);
+    res.status(500).json({ msg: 'Server Error!' });
+  }
+});
+
+// @route    POST api/auth
+// @desc     Authenticate user & get token
+// @access   Public
+
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required!').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // using destructuring to pull response's body information in one array for convenience
+
+    const { email, password } = req.body;
+
+    try {
+      // Check if user (trying to log in) exists
+
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials!' }] });
+      }
+
+      //User exists, but need to check if passwords match
+
+      const isMatch = await bcrypt.compare(password, user.password); //password is the one entered by user, user.password is the encrypted password of that user
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials!' }] });
+      }
+
+      // Return jsonwebtoken
+
+      const payload = {
+        user: { id: user.id }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
