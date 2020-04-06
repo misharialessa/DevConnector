654a3911a623b048a4658ade0a113dc4823fbf1e
Mishari <shamiya@gmail.com>
2020-04-06 21:18:18 +0300

    React Routes, Register Form, and useState Hook

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,60 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  //Using "spread" operation (...) to extract each item of the array as a seperate argumnt to the function
+  //Using [e.target.name] to be able to use onChange function for every "hook". so depending on what is being changed (the target of event e), onChange will change the value of that field depending on the name of that target
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('Success, Should be changed tho');
+  };
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Sign Into Your Account
+      </p>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Login;
