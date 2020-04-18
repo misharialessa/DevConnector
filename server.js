b207f065e39395b450fa8cccedf7bcd98b79c3d6
Mishari <shamiya@gmail.com>
2020-04-18 18:42:28 +0300

    Production changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 //Connect to database
@@ -11,7 +11,7 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API is Running!'));
+//app.get('/', (req, res) => res.send('API is Running!'));
 
 // Define Routes
 
@@ -19,6 +19,18 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+//Serve static assets in production
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+
+  app.use(express.statis('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
